4670852052506b3ccc42118fbf0437abcf8c91f5
LDaryaI <96127688+LDaryaI@users.noreply.github.com>
2022-05-15 03:14:53 +0300

    Занятие 4

--- 4-module/2-task/index.js
+++ 4-module/2-task/index.js
@@ -1,3 +1,11 @@
-function makeDiagonalRed(table) {
-  // ваш код...
-}
+let table = document.querySelector('table'),
+  makeDiagonalRed = tab => {
+    let i = 0;
+    [...tab.rows].forEach(tr => {
+      if(i === tr.cells.length) i = 0;
+      tr.cells[i].style.background = 'red';
+      i++;
+    });
+  };
+
+makeDiagonalRed(table);
