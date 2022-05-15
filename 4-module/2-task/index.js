9e6100a351577eb5906a2027d50d0f0a6e02cb39
LDaryaI <96127688+LDaryaI@users.noreply.github.com>
2022-05-15 13:03:26 +0300

    Исправила ошибку

--- 4-module/2-task/index.js
+++ 4-module/2-task/index.js
@@ -1,11 +1,5 @@
-let table = document.querySelector('table'),
-  makeDiagonalRed = tab => {
-    let i = 0;
-    [...tab.rows].forEach(tr => {
-      if(i === tr.cells.length) i = 0;
-      tr.cells[i].style.background = 'red';
-      i++;
-    });
-  };
-
-makeDiagonalRed(table);
+function makeDiagonalRed(table) {
+  [...table.rows].forEach((row, index) => {
+    row.cells[index].style.backgroundColor = 'red';
+  });
+}
