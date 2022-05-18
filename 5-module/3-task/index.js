281fc1e3d5ba205f3673c553fda675b21e29cab9
LDaryaI <96127688+LDaryaI@users.noreply.github.com>
2022-05-18 14:13:59 +0300

    Занятие 5

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,42 @@
+function showArrow(number, maxNumber, arrows) {
+  switch (number) {
+    case 1:
+      arrows[1].style.display = 'none';
+      break;
+    case maxNumber:
+      arrows[0].style.display = 'none';
+      break;
+    default:
+      arrows.forEach((arrow) => {
+        arrow.style.display = '';
+      });
+  }
+}
+
 function initCarousel() {
-  // ваш код...
+  const carouselInner = document.querySelector('.carousel .carousel__inner');
+  const carouselArrow = document.querySelectorAll('.carousel .carousel__arrow');
+
+  const carouselWidth = carouselInner.offsetWidth;
+  const maxSlideNumber = document.querySelectorAll('.carousel .carousel__slide').length;
+
+  let slideNumber = 1;
+  let sizeCarousel = 0;
+
+  carouselArrow.forEach((arrow) => {
+    arrow.addEventListener('click', () => {
+      if (arrow.classList.contains('carousel__arrow_right')) {
+        sizeCarousel -= carouselWidth;
+        slideNumber += 1;
+      } else {
+        sizeCarousel += carouselWidth;
+        slideNumber -= 1;
+      }
+
+      carouselInner.style.transform = `translateX(${sizeCarousel}px)`;
+      showArrow(slideNumber, maxSlideNumber, carouselArrow);
+    });
+  });
+
+  showArrow(slideNumber, maxSlideNumber, carouselArrow);
 }
